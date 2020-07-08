465cb9974e263c921583a091431607b8d5b344ee
greg <wegory@users.noreply.github.com>
2020-07-08 22:09:09 +0800

    Update styles.js

--- src/styles.js
+++ src/styles.js
@@ -1,17 +1,12 @@
-import { StyleSheet, Dimensions } from "react-native";
-var deviceWidth = Dimensions.get("window").width;
-
-
+import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
   header: {
     height: 60,
-    borderBottomWidth: 1,
-    borderColor: "gold",
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
-    backgroundColor: "#2E2E2E"
+    backgroundColor: "#FFF"
   },
   buttonAction: {
     height: "100%",
@@ -19,19 +14,9 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center"
   },
-  buttonText: {
-    fontSize: 19,
-    color: "gold",
-    fontWeight: "500"
-  },
-  buttonTextCancel: {
-    color: "white",
-    fontWeight: "400",
-    fontSize: 18
-  },
   body: {
     flexDirection: "row",
-    backgroundColor: "#1c2126"
+    backgroundColor: "#FFF"
   },
   picker: {
     flex: 1
